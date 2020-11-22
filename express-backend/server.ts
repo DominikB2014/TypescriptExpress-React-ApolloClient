19850cb935f597609928d5319ad133b66725ced8
Dominik Buszowiecki <Dominik.Buszowiecki@caseware.com>
2020-11-22 15:28:03 -0500

    remove nest

--- express-backend/server.ts
+++ express-backend/server.ts
@@ -1,14 +1,21 @@
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
 
 const app = express();
 const port = 8080;
 
-app.listen(port, () => {
-    console.log('server has started');
-})
 
 app.get('/', (req,res) => {
     res.send({
         json: 'hi'
     })
+})
+
+app.use('/graphql', graphqlHTTP({
+    schema: undfined,
+    graphiql: true
+}))
+
+app.listen(port, () => {
+    console.log('server has started');
 })
